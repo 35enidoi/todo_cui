95912f21aa2676a48f8ae726df484cbdb3c07142
いおぢね <143810964+35enidoi@users.noreply.github.com>
2025-02-06 23:52:32 +0900

    引数をparseするように

--- todo_app_test/todotest/ui/view.py
+++ todo_app_test/todotest/ui/view.py
@@ -2,7 +2,7 @@ from cmd import Cmd
 from datetime import datetime
 
 from todotest.model import TodoModel
-from todotest.utils.ui import trim_str, error_text
+from todotest.utils.ui import trim_str, error_text, parse_args
 
 
 class View(Cmd):
@@ -12,7 +12,38 @@ class View(Cmd):
         super().__init__()
         self.model = model
 
-    def do_show(self, _):
+    def emptyline(self):
+        return
+
+    def do_help(self, arg):
+        commands = [i[3:] for i in self.get_names() if i[:3] == "do_"]
+
+        # help単体の時の挙動
+        if arg == "":
+            print("avaible commands")
+            print("="*15)
+            print(" ".join(commands))
+            return
+
+        arg_names = (
+            (("command",), {"help": "command name"}),
+        )
+        args = parse_args(arg, prog="help", args=arg_names, description="show command help message")
+        if args is None:
+            return
+
+        if args.command in commands:
+            func = getattr(self, "do_" + args.command)
+            func("--help")
+        else:
+            print(error_text(f"command `{args.command}` is not available."))
+
+    def do_show(self, args):
+        arg_names = ()
+        args = parse_args(args, prog="show", args=arg_names, description="show Todos")
+        if args is None:
+            return
+
         tasks = self.model.show_tasks()
 
         if len(tasks) == 0:
@@ -34,17 +65,22 @@ class View(Cmd):
             print(text)
 
     def do_create(self, arg: str):
-        args = arg.split(" ")
-
-        if arg == "":
-            print(error_text("no args"))
+        arg_names = (
+            (("name",), {"help": "Todo name"}),
+            (("-d", "--description"), {"default": "", "help": "Todo description"})
+        )
+        args = parse_args(arg, prog="create", args=arg_names, description="create Todo")
+        if args is None:
             return
 
-        name = args[0]
-        description = " ".join(args[1:])
+        name = args.name
+        description = args.description
         created_todo = self.model.create_task(name, description)
         print("create successful!")
         print(f"todo id: {created_todo['id']}")
 
-    def do_exit(self, _):
+    def do_exit(self, arg):
+        if arg == "--help":
+            print("exit: exit this program.")
+            return
         return True
--- todo_app_test/todotest/utils/ui.py
+++ todo_app_test/todotest/utils/ui.py
@@ -1,3 +1,8 @@
+import shlex
+import argparse
+from typing import Union, Optional
+
+
 def trim_str(string: str, limit: int) -> str:
     """文字を指定の長さに整える(中央揃え)"""
     if len(string) > limit:
@@ -13,3 +18,25 @@ def trim_str(string: str, limit: int) -> str:
 def error_text(string) -> str:
     """エラー文字の定型文"""
     return "error has occured: " + string
+
+
+def parse_args(
+        string: str,
+        prog: str,
+        args: tuple[tuple[tuple[str], Union[None, dict]]],
+        description: Optional[str] = None) -> Union[None, argparse.Namespace]:
+    """引数をargparseするやつ。もしparseに失敗(変なarg)だと自動で文字を出力してNoneを返す"""
+    # 引数取り出し
+    row_args = shlex.split(string)
+
+    # フィルター作成
+    parser = argparse.ArgumentParser(prog=prog, description=description)
+    for names, keys in args:
+        if keys is None:
+            keys = dict()
+        parser.add_argument(*names, **keys)
+
+    try:
+        return parser.parse_args(row_args)
+    except SystemExit:
+        return
