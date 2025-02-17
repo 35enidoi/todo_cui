42b6c931c34817acfdca6c2fec0ead61aa97bde1
いおぢね <143810964+35enidoi@users.noreply.github.com>
2025-02-18 03:52:02 +0900

    detabaseのテストに文字いっぱいつけた

--- todo_app_test/tests/test_detabase.py
+++ todo_app_test/tests/test_detabase.py
@@ -5,52 +5,66 @@ from util import SQLChecker, TestCasewithTmpDB
 
 class DetabaseTestCase(TestCasewithTmpDB):
     def test_search(self):
-        # テスト値を追加
+        # 作成
         real_todo = self.sql_create_todo(self.tmp_file.name)
 
         # KEY
         for i in VALID_KEYS:
             todo = self.db.search_todo((i, real_todo[i]))[0]
-            self.assertEqual(todo, real_todo)
+            self.assertDictEqual(todo, real_todo)
 
-        # テスト値を追加
+        # さらに作成
         real_todos = [real_todo]
         for _ in range(3):
             real_todos.append(self.sql_create_todo(self.tmp_file.name))
 
         # ORDER
-        self.assertEqual(self.db.search_todo(order=("id", "ASC")), real_todos)
-        self.assertEqual(self.db.search_todo(order=("id", "DESC")), list(reversed(real_todos)))
+        self.assertListEqual(self.db.search_todo(order=("id", "ASC")), real_todos)
+        self.assertListEqual(self.db.search_todo(order=("id", "DESC")), list(reversed(real_todos)))
 
         # LIMIT
-        self.assertEqual(self.db.search_todo(order=("id", "ASC"), limit=2), real_todos[:2])
+        self.assertListEqual(self.db.search_todo(order=("id", "ASC"), limit=2), real_todos[:2])
 
     def test_create(self):
+        # 作成
         todo = self.db.create_todo("hoge", "huga")
 
+        # 確認
         with SQLChecker(self.tmp_file.name) as sql:
             for i in VALID_KEYS:
                 real_todo = self.todo_translate(*sql.execute(f"SELECT * FROM todos WHERE {i} = ?", todo[i])[0])
-                self.assertEqual(todo, real_todo)
+                self.assertDictEqual(todo, real_todo)
 
     def test_exist(self):
-        self.assertFalse(self.db.exist_todo("id", 1))
-
+        # 作成
         todo = self.sql_create_todo(self.tmp_file.name)
 
+        # 確認
         for i in VALID_KEYS:
             self.assertTrue(self.db.exist_todo(i, todo[i]))
+
+        # 削除
+        with SQLChecker(self.tmp_file.name) as sql:
+            sql.execute("DELETE FROM todos WHERE id = ?", todo["id"])
+
+        # 確認
+        for i in VALID_KEYS:
+            self.assertFalse(self.db.exist_todo(i, todo[i]))
 
     def test_update(self):
         ...  # Todo あとでつくる
 
     def test_complete(self):
+        # 作成
         todo = self.sql_create_todo(self.tmp_file.name)
 
+        # 完了してないことを確認
         self.assertFalse(todo["completed"])
 
+        # 完了
         self.db.complete_todo(todo["id"])
 
+        # 完了したことを確認
         with SQLChecker(self.tmp_file.name) as sql:
             completed_todo = self.db.todo_translate(*sql.execute("SELECT * FROM todos WHERE id = ?", todo["id"])[0])
             self.assertTrue(completed_todo["completed"])
